d0ab36ebe1563bec0328800fbdcf4754a768f7ec
lesgaag <leslievandergaag@gmail.com>
2022-03-12 15:50:04 +0100

    add typing for typescript

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { EventService } from '../shared/events.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from '../shared/index';
 
 @Component({
  templateUrl: './events-details.component.html',
@@ -10,7 +11,7 @@ import { ActivatedRoute } from '@angular/router';
  `]
 })
 export class EventDetailsComponent {
-    event: any;
+    event: IEvent;
     constructor(private eventService: EventService, private route: ActivatedRoute) {
     }
     // tslint:disable-next-line:use-life-cycle-interface
