5765cf4864031b419ca8b90014a59c582ddac38b
lesgaag <leslievandergaag@gmail.com>
2022-03-12 16:01:47 +0100

    Added template-based form

--- src/app/user/user.routes.ts
+++ src/app/user/user.routes.ts
@@ -1,6 +1,8 @@
+import { LoginComponent } from './login.component';
 import { ProfileComponent } from './profile.component';
 
 export const userRoutes = [
-    // path will be /user/profile
-    { path: 'profile', component: ProfileComponent }
+    // path will be /user/..
+    { path: 'profile', component: ProfileComponent },
+    { path: 'login', component: LoginComponent}
 ];
